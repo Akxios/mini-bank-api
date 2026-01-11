d04319970c2b12f954f420b6dac0d38afe30da56
Akxios <artemermolaev466@gmail.com>
2026-01-11 16:36:10 +0900

    Update: schemas/users.py

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,5 +1,15 @@
 from fastapi import Form
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, Field, model_validator, EmailStr
 
 
+class UserCreate(BaseModel):
+    email: EmailStr = Field(..., description="Почта пользователя")
+    password: str = Field(..., min_length=8, max_length=64, description="Пароль пользователя")
 
+
+class UserOut(BaseModel):
+    id: int = Field(..., description="Индефикатор пользователя")
+    email: EmailStr = Field(..., description="Почта пользователя")
+
+    class Config:
+        from_attributes = True
